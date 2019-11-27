dc6412c080ce5355efc0374c3d6649704bd61b58
Safdar Nazir <44896417+Safdar-Nazir@users.noreply.github.com>
2019-11-27 16:19:25 +0500

    Add files via upload

---
+++ Basics/Arrays.h
@@ -0,0 +1,16 @@
+//
+// Created by Safdar Nazir on 11/27/2019.
+//
+
+#ifndef BASICS_ARRAYS_H
+#define BASICS_ARRAYS_H
+
+
+class Arrays {
+public:
+    void Array();
+
+};
+
+
+#endif //BASICS_ARRAYS_H
